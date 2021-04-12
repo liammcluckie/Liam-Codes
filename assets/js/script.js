a91a715c5cf3cc496f28db830d8e12a9f95a70c3
Liam McLuckie <liammcluckie@hotmail.co.uk>
2021-04-12 17:23:29 +0100

    Add functions for sm device nav

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Display search bar and nav bar on small devices
+
+const menuIcon = document.getElementById("menu-icon");
+const slideOutMenu = document.getElementById("slideout-menu");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("search-box");
+
+searchIcon.addEventListener('click', function() {
+    if(searchBox.style.top == '72px') {
+        searchBox.style.top = '24px';
+        searchBox.style.pointerEvents = 'none';
+    } else {
+        searchBox.style.top = '72px';
+        searchBox.style.pointerEvents = 'auto';
+    }
+});
+
+menuIcon.addEventListener('click', function() {
+    if(slideOutMenu.style.opacity == '1') {
+        slideOutMenu.style.opacity = '0';
+        slideOutMenu.style.pointerEvents = 'none';
+    } else {
+        slideOutMenu.style.opacity = '1';
+        slideOutMenu.style.pointerEvents = 'auto';
+    }
+});
